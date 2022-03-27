1329d604087739b426b1050b35e4696eacdc49ab
JudyWu <judywu@sat.cool>
2022-03-27 15:44:57 +0800

    feat(todolist): 新增狀態顏色樣式

--- resources/views/todolist.blade.php
+++ resources/views/todolist.blade.php
@@ -24,7 +24,7 @@
                     <button type="button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" id="createBtn" onclick="addTodo()">新增</button>
                 </form>
             </div>
-           
+
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <table class="table-fixed">
@@ -43,7 +43,11 @@
                             <tr>
                                 <td>{{ $task->title }}</td>
                                 <td>{{ $task->description }}</td>
-                                <td class="text-center">{{ $task->completed ? '完成' : '未完成' }}</td>
+                                @if($task->completed)
+                                    <td class="text-center text-green-600">完成</td>
+                                @else
+                                    <td class="text-center text-gray-400">未完成</td>
+                                @endif
                                 <td>
                                     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" id="updateStatusBtn"
                                             onclick="updateStatus('{{ $task->id }}', '{{ $task->completed }}')">
